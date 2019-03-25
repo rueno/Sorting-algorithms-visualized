c07b85bea5e80a1dc81cb6ff06133839a9bf5880
rueno <77simi@web.de>
2019-03-25 17:47:53 +0100

    Improved code for generating the Chart

--- src/me/rueno/Sortingalgorithms/UI/Dialog/CompareAlgosDialog.java
+++ src/me/rueno/Sortingalgorithms/UI/Dialog/CompareAlgosDialog.java
@@ -3,6 +3,8 @@ package me.rueno.Sortingalgorithms.UI.Dialog;
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map.Entry;
 
 import javax.swing.JDialog;
 import javax.swing.JFrame;
@@ -19,10 +21,12 @@ import org.jfree.data.xy.XYSeriesCollection;
 
 import me.rueno.Sortingalgorithms.Lists.ListGenerator;
 import me.rueno.Sortingalgorithms.Lists.ListType;
+import me.rueno.Sortingalgorithms.Logic.ISortingAlgorithm;
 import me.rueno.Sortingalgorithms.Logic.Algorithms.BubbleSort;
 import me.rueno.Sortingalgorithms.Logic.Algorithms.InsertionSort;
 import me.rueno.Sortingalgorithms.Logic.Algorithms.QuickSort;
 import me.rueno.Sortingalgorithms.Logic.Algorithms.SelectionSort;
+import me.rueno.Sortingalgorithms.Logic.Algorithms.ShellSort;
 
 public class CompareAlgosDialog extends JDialog{
 	
@@ -36,9 +40,10 @@ public class CompareAlgosDialog extends JDialog{
 		setTitle("Algorithmen im Vergleich");
 		setSize(657, 436);
 		
-		XYDataset set = createSet();
+		XYDataset set = createSet(new BubbleSort(null), new SelectionSort(null), new InsertionSort(null),
+				new QuickSort(null), new ShellSort(null));
 		
-		chart = ChartFactory.createXYLineChart("Sortieralgorithmen im Vergleich (Liste mit zufälliger Anordnung)", "Anzahl Elemente",
+		chart = ChartFactory.createXYLineChart("Sortieralgorithmen im Vergleich (Integer mit zufälliger Anordnung)", "Anzahl Elemente",
 				"Benötigte Zeit in Sekunden", set, PlotOrientation.VERTICAL, true, true, false);
 		ChartPanel panel = new ChartPanel(chart);
 		panel.setDisplayToolTips(true);
@@ -49,48 +54,35 @@ public class CompareAlgosDialog extends JDialog{
 		render.setSeriesPaint(1, Color.BLUE);
 		render.setSeriesPaint(2, Color.YELLOW);
 		render.setSeriesPaint(3, Color.GREEN);
+		render.setSeriesPaint(4, Color.ORANGE);
 		render.setSeriesStroke(0, new BasicStroke(0.75F));
 		render.setSeriesStroke(1, new BasicStroke(0.75F));
 		render.setSeriesStroke(2, new BasicStroke(0.75F));
 		render.setSeriesStroke(3, new BasicStroke(0.75F));
+		render.setSeriesStroke(4, new BasicStroke(0.75F));
 		plot.setRenderer(render);
 		setContentPane(panel);
 	}
 	
-	private XYDataset createSet(){
-		XYSeries bubble = new XYSeries("Bubblesort");
-		bubble.add(0, 0);
+	private XYDataset createSet(ISortingAlgorithm... algos){
+		XYSeriesCollection collection = new XYSeriesCollection();
 		
-		XYSeries insertion = new XYSeries("Insertionsort");
-		insertion.add(0, 0);
+		HashMap<ISortingAlgorithm, XYSeries> map = new HashMap<ISortingAlgorithm, XYSeries>();
 		
-		XYSeries selection = new XYSeries("Selectionsort");
-		selection.add(0, 0);
-		
-		XYSeries quick = new XYSeries("Quicksort");
-		quick.add(0, 0);
-		
-		ListGenerator gen = new ListGenerator();
-		
-		BubbleSort bubbleSort = new BubbleSort(null);
-		InsertionSort insertionSort = new InsertionSort(null);
-		SelectionSort selectionSort = new SelectionSort(null);
-		QuickSort quickSort = new QuickSort(null);
-		
-		for(int i = 5000; i <= 50000; i += 5000){
-			Integer[] array = gen.generateIntegerList(i, ListType.RANDOM);
-			
-			bubble.add(i, bubbleSort.measureAlgorithm(copyArray(array))[0] / 1000.0F);
-			insertion.add(i, insertionSort.measureAlgorithm(copyArray(array))[0] / 1000.0F);
-			selection.add(i, selectionSort.measureAlgorithm(copyArray(array))[0] / 1000.0F);
-			quick.add(i, quickSort.measureAlgorithm(copyArray(array))[0] / 1000.0F);
+		for(ISortingAlgorithm algo: algos){
+			XYSeries series = new XYSeries(algo.getName());
+			series.add(0, 0);
+			map.put(algo, series);
+			collection.addSeries(series);
 		}
 		
-		XYSeriesCollection collection = new XYSeriesCollection();
-		collection.addSeries(bubble);
-		collection.addSeries(insertion);
-		collection.addSeries(selection);
-		collection.addSeries(quick);
+		ListGenerator gen = new ListGenerator();
+		for(int i = 5000; i <= 50000; i += 5000){
+			Integer[] array = gen.generateIntegerList(i, ListType.RANDOM);
+			for(Entry<ISortingAlgorithm, XYSeries> entry: map.entrySet()){
+				entry.getValue().add(i, entry.getKey().measureAlgorithm(copyArray(array))[0] / 1000.0F);
+			}
+		}
 		
 		return collection;
 	}
