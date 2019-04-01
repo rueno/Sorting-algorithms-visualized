433d86e1a1a6ead265550642893a1f7e00d1448f
rueno <77simi@web.de>
2019-04-01 19:31:16 +0200

    Improved animated labels

--- src/me/rueno/Sortingalgorithms/UI/Components/AnimatedImageLabel.java
+++ src/me/rueno/Sortingalgorithms/UI/Components/AnimatedImageLabel.java
@@ -13,7 +13,7 @@ public abstract class AnimatedImageLabel extends JLabel{
 	
 	private static final long serialVersionUID = -6238097470939913778L;
 	private int currentImage;
-	private boolean interrupted, terminated;
+	private boolean interrupted;
 	private ImageIcon[] images;
 	
 	private ScheduledFuture<?> future;
@@ -21,7 +21,6 @@ public abstract class AnimatedImageLabel extends JLabel{
 	public AnimatedImageLabel(String resourcePrefix, int delay, int imageAmount, int width, int height, boolean preload){
 		this.currentImage = 0;
 		this.interrupted = false;
-		this.terminated = false;
 		if(preload){
 			images = new ImageIcon[imageAmount];
 			for(int i = 0; i < imageAmount; i++){
@@ -31,34 +30,24 @@ public abstract class AnimatedImageLabel extends JLabel{
 			this.setIcon(images[0]);
 		}
 		
-		future = GlobalVars.scheduler.schedule(() -> {
-			while(!terminated){
-				while(!interrupted){
-					try{
-						ImageIcon next;
-						
-						if(preload){
-							next = images[currentImage];
-						}else{
-							Image scaled = new ImageIcon(AnimatedImageLabel.class.getResource(resourcePrefix + currentImage + ".png")).getImage()
-									.getScaledInstance(width, height, Image.SCALE_SMOOTH);
-							next = new ImageIcon(scaled);
-						}
-						this.currentImage = (currentImage + 1) % imageAmount;
-						this.setIcon(next);
-						
-						repaint();
-						
-						Thread.sleep(delay);
-					}catch(InterruptedException interrupted){}
+		future = GlobalVars.scheduler.scheduleAtFixedRate(() -> {
+			if(!interrupted){
+				ImageIcon next;
+				
+				if(preload){
+					next = images[currentImage];
+				}else{
+					Image scaled = new ImageIcon(AnimatedImageLabel.class.getResource(resourcePrefix + currentImage + ".png")).getImage()
+							.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+					next = new ImageIcon(scaled);
 				}
-				try{
-					Thread.sleep(100);
-				}catch(InterruptedException interrupted){
-					return;
-				}
+				this.currentImage = (currentImage + 1) % imageAmount;
+				this.setIcon(next);
+				
+				repaint();
 			}
-		}, 0, TimeUnit.MILLISECONDS);
+		}, 0, delay, TimeUnit.MILLISECONDS);
+		
 	}
 	
 	public void setInterrupted(boolean interrupt){
@@ -67,8 +56,6 @@ public abstract class AnimatedImageLabel extends JLabel{
 	
 	
 	public void terminate(){
-		this.setInterrupted(true);
-		this.terminated = true;
-		future.cancel(true);
+		future.cancel(false);
 	}
 }
