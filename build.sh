55df6b3c587bd80a73667b85199538a3f55c789c
rueno <77simi@web.de>
2019-11-29 22:32:59 +0100

    🍕

--- build.sh
+++ build.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-if [ ! -d "target"]
-then
-    echo "Target directory not found. Creating..."
-    mkdir ./target
-    echo "File created."
-fi
-
-cd $TRAVIS_BUILD_DIR/target
 mvn install -DskipTests=true -Dmaven.javadoc.skip=true -B -V
